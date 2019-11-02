c9608a18db1708bf4249ee525add2a35043e0d93
Bryan Jenks <thenewpheonix@gmail.com>
2019-11-01 17:09:05 -0700

    got a working C3 chart

---
+++ js/blogindex.js
@@ -0,0 +1,17 @@
+var chart = c3.generate({
+    bindto: '#chart1',
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    }
+});
