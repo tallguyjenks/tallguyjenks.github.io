6381f4ddafb2065247824c2fb5873922f9f6cff9
Bryan Jenks <thenewpheonix@gmail.com>
2019-11-08 20:39:01 -0800

    got a chart working for blog post metrics

---
+++ js/blog.js
@@ -0,0 +1,27 @@
+// Chart
+var chart = c3.generate({
+    bindto: "#chart",
+    data: 
+    {
+        columns: 
+        [
+            ['Date', 201901, 201902, 201903, 201904, 201905, 201906],
+            ['2019', 30, 200, 100, 400, 150, 250]
+        ],
+        x: 'Date',
+        labels: true,
+        colors: 
+        {
+            '2019': '#d79921',
+            // TODO put the rest of the guvbox colors in here for additional years
+        },
+    },
+    zoom: 
+    {
+        enabled: true
+    },
+    point: 
+    {
+        r: 5
+    },
+});
